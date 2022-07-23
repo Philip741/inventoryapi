7bfe3079621352d12046134e6e7356c7ba6ce78d
Philip Browning <philip741@gmail.com>
2022-07-23 16:57:52 -0400

    Moved invapi to main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
 from fastapi import FastAPI
-
+from .database import SessionLocal, engine
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+
