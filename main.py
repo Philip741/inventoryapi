c09f54d9bb2fa0ffba8cb72eec860110cf312904
Philip Browning <philip741@gmail.com>
2022-07-25 19:02:19 -0400

    Modified import statements

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import sys
-from fastapi import FastAPI
-from .database import SessionLocal, engine
+from fastapi import FastAPI, status
+import models
+#import schemas
+from database import Base, engine
+
+Base.metadata.create_all(engine)
 
 app = FastAPI()
 
