fdfaf85fa4113458a12ef1a922c7bb051fc213d6
Philip Browning <philip741@gmail.com>
2022-07-25 19:01:27 -0400

    Comment out session statements until I can better understand how they work

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+#from sqlalchemy.orm import sessionmaker
 
 #SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONN")
 
 engine = create_engine('sqlite:///cmdb.db')
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
